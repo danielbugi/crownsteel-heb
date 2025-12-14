d5cd599240753d8111124f94d266f5b0067be214
danielbugi <dabogatesting@gmail.com>
2025-12-14 22:26:42 +0200

    Add eslint disable comment for adapter type assertion

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from '@/lib/prisma';
 import bcrypt from 'bcryptjs';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   adapter: PrismaAdapter(prisma) as any,
   trustHost: true, // Fix for UntrustedHost error
   providers: [
